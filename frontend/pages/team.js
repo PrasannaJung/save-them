b6cc931021ddee671f5a3ecd73d2d6b8b04048ea
Saurabh Patil <55076843+Saurabhpatil-dev@users.noreply.github.com>
2022-11-19 05:01:12 +0530

    Adding the images

--- frontend/pages/team.js
+++ frontend/pages/team.js
@@ -1,34 +1,34 @@
-import React from "react";
-import Team from "../components/Team";
-import styles from "../styles/Team.module.css";
+import React from 'react';
+import Team from '../components/Team';
+import styles from '../styles/Team.module.css';
 
 const TeamPage = () => {
   return (
     <>
       <div class={`${styles.profiles} px-4`}>
         <Team
-          name='Prasanna'
-          role='Frontend Developer'
-          url={"/pp.jpg"}
-          socialLink={"https://twitter.com/prasannaj_dev"}
+          name="Prasanna"
+          role="Frontend Developer"
+          url={'/pp.jpg'}
+          socialLink={'https://twitter.com/prasannaj_dev'}
         />
         <Team
-          name='Rahul'
-          role='UI/UX Designer'
-          url='/photo.jpeg'
-          socialLink={"https://twitter.com/rahul3526"}
+          name="Rahul"
+          role="UI/UX Designer"
+          url="/rahul.jpg"
+          socialLink={'https://twitter.com/rahul3526'}
         />
         <Team
-          name='Saurabh'
-          role='Backend Developer'
-          url='/photo.jpeg'
-          socialLink={"https://twitter.com/saurabh17999"}
+          name="Saurabh"
+          role="Backend Developer"
+          url="/ssp.jpeg"
+          socialLink={'https://twitter.com/saurabh17999'}
         />
         <Team
-          name='Neha'
-          role='NFT Artist'
-          url='/photo.jpeg'
-          socialLink={"https://twitter.com/nehathecoder"}
+          name="Neha"
+          role="NFT Artist"
+          url="/neha.jpg"
+          socialLink={'https://twitter.com/nehathecoder'}
         />
       </div>
     </>
