ccef16d4d39107cf50fd8d6ae75f120a411d4a72
Saurabh Patil <55076843+Saurabhpatil-dev@users.noreply.github.com>
2022-11-19 05:09:06 +0530

    Making changes in Footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { FaGithub } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className=' bg-[#251d45] text-white flex items-center justify-between flex-col gap-6 medium:flex-row px-10 py-2 sticky top-full'>
-      <div className='flex flex-col items-center'>
-        <h1 className='text-2xl font-bold'>@Save Them, 2022</h1>
-        <span className='text-xs font-semibold'> All Rights Reserved</span>
+    <footer className=" bg-[#251d45] text-white flex items-center justify-between flex-col gap-6 medium:flex-row px-10 py-2 sticky top-full">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold">@PAWSITIVE, 2022</h1>
+        <span className="text-xs font-semibold">All Rights Reserved</span>
       </div>
-      <ul className='pr-5 text-3xl flex gap-6'>
+      {/* <ul className='pr-5 text-3xl flex gap-6'>
         <li className='hover:text-primaryGradientTwo transition-colors cursor-pointer'>
           <FaGithub />
         </li>
@@ -22,7 +22,7 @@ const Footer = () => {
         <li className='hover:text-primaryGradientTwo transition-colors cursor-pointer'>
           <FaTwitter />
         </li>
-      </ul>
+      </ul> */}
     </footer>
   );
 };
