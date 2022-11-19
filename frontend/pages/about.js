3405eecde2a3ecbbf7924575f1edc27842421946
rahulsingh3526 <92939339+rahulsingh3526@users.noreply.github.com>
2022-11-19 05:57:49 +0530

    Update about.js

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -6,24 +6,13 @@ const AboutPage = () => {
       <div className='w-2/3 mx-auto mt-8 text-2xl'>
         <h1 className='font-bold text-3xl mb-3'>About Us</h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-          corrupti, eius assumenda placeat, quam ratione fugiat fuga minima sint
-          labore voluptas id corporis adipisci culpa, perspiciatis nisi at
-          doloribus maiores. Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Itaque animi quia at nostrum quam quasi. Nobis sunt totam
-          voluptatibus autem.
+       India is the rabies capital of the world with more than 70% of the world cases coming from here. The idea behind the project is to raise funds for the Dogs to help them with hospitality, food, and shelter and vasectomy. To cope with with the rising population of the stray dogs and stop the killing of these dogs with cruelty and to rectify the mistakes and move ahead towards the future and controlling the spread of the disease.
         </p>
       </div>
       <div className='w-2/3 mx-auto mt-16 mb-6 text-2xl'>
         <h1 className='font-bold text-3xl mb-3'>Our Goal</h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-          corrupti, eius assumenda placeat, quam ratione fugiat fuga minima sint
-          labore voluptas id corporis adipisci culpa, perspiciatis nisi at
-          doloribus maiores. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Voluptatum enim cupiditate sunt quisquam odit aspernatur
-          accusamus accusantium, illum voluptas hic dolorum minus ipsa error
-          voluptatem corporis tenetur? Dignissimos, consectetur enim?
+         Pawsitive is a platform where people can buy Nfts through which we raise funds for dogs.Now all the nft holders will be given a special voting right and they will be made into a DAO member where they can decide on which proposal to vote for how all the money collected by nft will be used towards finding the solution to this problem.
         </p>
       </div>
     </div>
